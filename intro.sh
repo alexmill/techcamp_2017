45498a3b9dd0ddf80851b66f0c492509055f6442
unknown <alexmill@OID526-4AM.wharton.upenn.edu>
2017-07-26 14:25:04 -0400

    R intro

--- intro.sh
+++ intro.sh
@@ -1,3 +1,4 @@
+url="http://artii.herokuapp.com/make?text=$1+is+a+Hacker"
 cat ./ascii.txt
-(curl http://artii.herokuapp.com/make?text=$1+is+a+Hacker 2> /dev/null) || (./win_curl/curl http://artii.herokuapp.com/make?text=$1+is+a+Hacker 2> /dev/null)
+(curl $url 2> /dev/null) || (./win_curl/curl $url 2> /dev/null)
 echo ""

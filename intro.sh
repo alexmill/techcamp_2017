fb77b783c0b01e7e5d532f7e393e1f679c2caf2d
unknown <alexmill@OID526-4AM.wharton.upenn.edu>
2017-07-26 10:41:14 -0400

    flip

--- intro.sh
+++ intro.sh
@@ -1,3 +1,3 @@
+cat ./ascii.txt
 (curl http://artii.herokuapp.com/make?text=$1+is+a+Hacker 2> /dev/null) || (./win_curl/curl http://artii.herokuapp.com/make?text=$1+is+a+Hacker 2> /dev/null)
 echo ""
-cat ./ascii.txt

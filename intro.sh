93daa329dba1bc691721ce63c7131915ca6fe706
unknown <alexmill@OID526-4AM.wharton.upenn.edu>
2017-07-26 10:39:21 -0400

    fix

--- intro.sh
+++ intro.sh
@@ -1,2 +1,3 @@
 (curl http://artii.herokuapp.com/make?text=$1+is+a+Hacker 2> /dev/null) || (./win_curl/curl http://artii.herokuapp.com/make?text=$1+is+a+Hacker 2> /dev/null)
 echo ""
+cat ./ascii.txt
